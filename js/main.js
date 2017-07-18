5f290798732f0665d05344e85fee9449bb23e839
Jenna-Smith <jennawsmith@outlook.com>
2017-07-17 20:26:26 -0400

    add padding to css part of ready function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     var paragraphs = $('p');
-    $(paragraphs).css({'color': '#000080', 'background-color': '#ff9843', 'font-size': '2rem'});
+    $(paragraphs).css({'color': '#000080', 'background-color': '#ff9843', 'font-size': '1.5rem', 'padding': '10px'});
 });
